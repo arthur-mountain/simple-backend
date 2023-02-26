14d2479a3c2c861561179f916006b0c45bab587b
arthur <arthur.oceanus@gmail.com>
2023-02-27 03:25:43 +0800

    fixed middleware log

--- internal/interactor/middleware/logs/log.go
+++ internal/interactor/middleware/logs/log.go
@@ -6,89 +6,131 @@ import (
 	"net/http"
 	"os"
 	"path"
-	errorUtils "simple-backend/internal/utils/error"
 	"time"
+
+	errorUtils "simple-backend/internal/utils/error"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 
-func NewLogger() *logrus.Logger {
-	now := time.Now() // current time
-	logPath := ""     // folder path
-	logFileName := now.Format("2006-01-02") + ".log"
+type tLogHook struct{}
 
-	// set up log path and file name
-	dir, err := os.Getwd()
-	if err == nil {
-		logPath = dir + "/logs/"
+// Notice: docker container always +0UTC
+func (h *tLogHook) Fire(entry *logrus.Entry) error {
+	timezoneStr := os.Getenv("TIMEZONE")
+	timezone, _ := time.LoadLocation(timezoneStr)
+
+	// set log timezone
+	entry.Time = entry.Time.In(timezone)
+	entry.Data["timezone"] = timezoneStr
+	// set log formatter
+	entry.Logger.Formatter = &logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		DisableColors:   true,
+		FullTimestamp:   true,
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime:  "@time",
+			logrus.FieldKeyLevel: "@level",
+			logrus.FieldKeyMsg:   "@message",
+		},
 	}
 
+	return nil
+}
+
+func (h *tLogHook) Levels() []logrus.Level {
+	if os.Getenv("ENVIRONMENT") == "production" {
+		return []logrus.Level{
+			logrus.InfoLevel,
+			logrus.WarnLevel,
+			logrus.ErrorLevel,
+			logrus.FatalLevel,
+			logrus.PanicLevel,
+		}
+	}
+
+	return logrus.AllLevels
+}
+
+func NewLogger() *logrus.Logger {
+	// get log path
+	logPath, err := os.Getwd()
+	if err != nil {
+		fmt.Println("new logger couldn't found current directory", err)
+	}
+
+	// create log directory
+	logPath = logPath + "/logs/"
 	err = os.MkdirAll(logPath, 0777)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println("create log directory", err)
 	}
 
-	fileName := path.Join(logPath, logFileName)
-
 	// check log file path is exists
-	_, err = os.Stat(fileName)
-	if err != nil {
-		_, err = os.Create(fileName)
-		if err != nil {
-			fmt.Println(err.Error())
+	fileName := path.Join(
+		logPath,                                // log path
+		time.Now().Format("2006-01-02")+".log", // log filename
+	)
+	if _, err = os.Stat(fileName); err != nil {
+		if _, err = os.Create(fileName); err != nil {
+			fmt.Println("create file error: ", err)
 		}
 	}
 
 	// open log file
-	src, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
-	if err != nil {
-		fmt.Println("open log file err: ", err)
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if errors.Is(err, os.ErrNotExist) {
+		fmt.Println("open log file err", err)
 	}
 
 	// create logrus instance
 	logger := logrus.New()
-	logger.Out = src
-	logger.SetLevel(logrus.DebugLevel)
-	// log time is +0
-	logger.SetFormatter(&logrus.TextFormatter{
-		TimestampFormat: "2006-01-02 15:04:05",
-	})
+	logger.SetOutput(file)        // append log to file
+	logrus.SetReportCaller(true)  // 測試環境，將檔案名稱和位置一同紀錄
+	logger.AddHook(new(tLogHook)) // invoked before append log to file
 
 	return logger
+}
+
+func handleRecoverLog(c *gin.Context, logger *logrus.Logger) {
+	if err := recover(); err != nil {
+		customError := errorUtils.NewCustomError(
+			errors.New("please contact the administrator"),
+			http.StatusInternalServerError,
+		)
+		c.AbortWithStatusJSON(customError.HttpStatusCode, customError)
+
+		statusCode := c.Writer.Status()
+		reqUri := c.Request.RequestURI
+		reqMethod := c.Request.Method
+		clientIP := c.ClientIP()
+		logger.Errorf("code:%d | ip:%s | method:%s | uri:%s | message: %s",
+			statusCode,
+			clientIP,
+			reqMethod,
+			reqUri,
+			err,
+		)
+	}
 }
 
 func LoggerToFile() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		logger := NewLogger()
-		reqMethod := c.Request.Method
-		reqUri := c.Request.RequestURI
-		statusCode := c.Writer.Status()
-		clientIP := c.ClientIP()
-
-		defer func() {
-			if err := recover(); err != nil {
-				customError := errorUtils.NewCustomError(
-					errors.New("please contact the administrator"),
-					http.StatusInternalServerError,
-				)
-				logger.Errorf("code:%d | ip:%s | method:%s | uri:%s | message: %s",
-					statusCode,
-					clientIP,
-					reqMethod,
-					reqUri,
-					err,
-				)
-				c.AbortWithStatusJSON(customError.HttpStatusCode, customError)
-			}
-		}()
-
+		defer handleRecoverLog(c, logger)
 		c.Next()
-		logger.Infof("code:%d | ip:%s | method:%s | uri:%s",
-			statusCode,
-			clientIP,
-			reqMethod,
-			reqUri,
-		)
+
+		if httpCode := c.Writer.Status(); httpCode >= 400 {
+			reqUri := c.Request.RequestURI
+			reqMethod := c.Request.Method
+			clientIP := c.ClientIP()
+			logger.Errorf("code:%d | ip:%s | method:%s | uri:%s",
+				httpCode,
+				clientIP,
+				reqMethod,
+				reqUri,
+			)
+		}
 	}
 }
